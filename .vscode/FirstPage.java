399c24cd2824e21568f26fc8cf9760a838644e9d
AbdullahAljehani <aboode154j@gmail.com>
2023-11-19 20:57:48 +0300

    Update FirstPage.java

--- .vscode/FirstPage.java
+++ .vscode/FirstPage.java
@@ -3,8 +3,12 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class FirstPage extends Application {
@@ -15,16 +19,21 @@ public class FirstPage extends Application {
 
         // Create label
         Label titleLabel = new Label("Best Route to deliver package");
-        titleLabel.setStyle("-fx-font-size: 18px;");
+        titleLabel.setStyle("-fx-font-size: 22px; -fx-text-fill: white;"); // Set text color to white
+        titleLabel.setAlignment(Pos.CENTER);
 
         // Create buttons
         Button phase1Button = new Button("Phase 1");
         Button phase2Button = new Button("Phase 2");
 
+        // Make buttons bigger
+        phase1Button.setStyle("-fx-font-size: 20px;");
+        phase2Button.setStyle("-fx-font-size: 20px;");
+
         // Create GridPane for buttons
         GridPane gridPane = new GridPane();
         gridPane.setAlignment(Pos.CENTER);
-        gridPane.setHgap(20); // Set horizontal gap between buttons
+        gridPane.setHgap(40); // Set horizontal gap between buttons (increased from 20)
 
         // Add buttons to GridPane
         gridPane.add(phase1Button, 0, 0);
@@ -36,6 +45,11 @@ public class FirstPage extends Application {
         BorderPane.setAlignment(titleLabel, Pos.CENTER);
         borderPane.setCenter(gridPane);
 
+        // Set dark mode background color
+        BackgroundFill backgroundFill = new BackgroundFill(Color.DARKSLATEGRAY, CornerRadii.EMPTY, javafx.geometry.Insets.EMPTY);
+        Background background = new Background(backgroundFill);
+        borderPane.setBackground(background);
+
         // Bind properties for dynamic resizing
         titleLabel.prefWidthProperty().bind(borderPane.widthProperty());
         titleLabel.prefHeightProperty().bind(borderPane.heightProperty().multiply(0.2)); // Example: 20% of the height
@@ -43,11 +57,11 @@ public class FirstPage extends Application {
         gridPane.prefHeightProperty().bind(borderPane.heightProperty().multiply(0.8)); // Example: 80% of the height
 
         // Set minimum width and height for the stage
-        primaryStage.setMinWidth(300); // Set your desired minimum width
-        primaryStage.setMinHeight(200); // Set your desired minimum height
+        primaryStage.setMinWidth(400); // Set your desired minimum width (increased from 300)
+        primaryStage.setMinHeight(300); // Set your desired minimum height
 
         // Create Scene
-        Scene scene = new Scene(borderPane, 300, 300);
+        Scene scene = new Scene(borderPane, 400, 300); // Adjusted the initial size
 
         // Set the scene to the stage
         primaryStage.setScene(scene);
@@ -56,5 +70,7 @@ public class FirstPage extends Application {
         primaryStage.show();
     }
 
-    
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
