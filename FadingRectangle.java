e980b310afc1ca7baa68f07d30a6c1dbccf49c76
AbdulrahmanOtb <abdulrahman.aldugailbi@gmail.com>
2023-11-19 21:27:03 +0300

    ..

--- FadingRectangle.java
+++ FadingRectangle.java
@@ -15,8 +15,6 @@ import javafx.scene.layout.Pane;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
