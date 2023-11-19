ffb3956eefdecde369e5b4f67d65391770a72649
AbdullahAljehani <aboode154j@gmail.com>
2023-11-19 19:05:49 +0300

    modify the timer

--- MainProgram.java
+++ MainProgram.java
@@ -1,42 +1,53 @@
 import java.util.Timer;
 import java.util.TimerTask;
-import java.util.concurrent.TimeUnit;
+import java.util.List;  
 
-public class MainProgram  {
+public class MainProgram {
+    private static DeliveryDriver deliveryDriver;  
     private static int secondsPassed = 0;
 
     public static void main(String[] args) {
-        // Create a timer
         Timer timer = new Timer();
 
-        // Create a TimerTask (you can implement your logic in this task)
+       
         TimerTask simulationTask = new TimerTask() {
             @Override
             public void run() {
                 secondsPassed++;
 
-                // Your simulation logic goes here
-                System.out.println("Timer" + formatTime(secondsPassed));
+                
+                System.out.println("Timer " + formatTime(secondsPassed));
 
-                // Check if the simulation has reached the desired duration (e.g., 10 minutes)
-                if (secondsPassed >= TimeUnit.MINUTES.toSeconds(10)) {
-                    System.out.println("Simulation completed.");
-                    cancel(); // Stop the timer task
-                    timer.cancel(); // Stop the timer
+               
+                if (allDelivered()) {
+                    System.out.println("All packages delivered. Simulation completed.");
+                    timer.cancel();
                 }
             }
         };
 
-        // Schedule the TimerTask to run every 100 milliseconds (adjust the delay and period as needed)
-        timer.schedule(simulationTask, 0, 10); // 0 ms delay, 100 ms period (0.1 seconds)
+        
+        timer.schedule(simulationTask, 0, 10); 
+    }
+
+  
+
+    private static boolean allDelivered() {
+        List<Package> packages = deliveryDriver.getPackages();
+
+        for (Package aPackage : packages) {
+            if (!aPackage.isDelivered) {
+                return false; 
+            }
+        }
+        return true; 
     }
 
     private static String formatTime(int seconds) {
-        long hours = TimeUnit.SECONDS.toHours(seconds);
-        long minutes = TimeUnit.SECONDS.toMinutes(seconds) % 60;
+        long hours = seconds / 3600;
+        long minutes = (seconds % 3600) / 60;
         long remainingSeconds = seconds % 60;
-    
+
         return String.format("%02d:%02d:%02d", hours, minutes, remainingSeconds);
     }
 }
-
